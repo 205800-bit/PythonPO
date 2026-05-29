0e87eb139514704d511e73bb42c1464113c2b6a9
205800-bit <205800@stmichaelcollege.nl>
2026-05-29 17:36:50 +0200

    Vragen quiz

--- POPhyton.py
+++ POPhyton.py
@@ -1,1 +1,17 @@
 #Spiderman villan Quiz
+
+""""De 10 vragen die in de quiz gaan komen"""
+
+vragen = [
+    "Welke motivatie past het meest bij jou?",
+    "Hoe pak jij een probleem aan?",
+    "Hoe ga je om met vijanden?",
+    "Welke kracht past het beste bij jou?",
+    "Hoe reageer je als iemand je onderschat?",
+    "Wat is jouw grootste zwakte?",
+    "Hoe werk je het liefst?",
+    "Wat zou je doen als je nieuwe krachten krijgt?",
+    "Hoe ga je om met verlies?",
+    "Wat vind je het belangrijkst in een gevecht?"
+]
+
