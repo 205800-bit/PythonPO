07b74634b4cf24f5b40eaa8562861ecc67dbf8c6
205800-bit <205800@stmichaelcollege.nl>
2026-05-29 18:06:39 +0200

    Antwoorden

--- POPhyton.py
+++ POPhyton.py
@@ -16,6 +16,68 @@ vragen = [
     "Wat vind je het belangrijkst in een gevecht?"
 ]
 
-=======
-#Spiderman Quiz
->>>>>>> c293ca1a3942b604bafdbc17e92a25f5e0de3326
+
+antwoorden = [
+    [
+        "Ik wil controle en kennis uitbreiden",
+        "Ik wil chaos creëren en macht tonen",
+        "Ik wil wraak en brute kracht gebruiken",
+        "Ik wil pure energie en kracht voelen"
+    ],
+    [
+        "Ik analyseer het en maak een plan",
+        "Ik ga impulsief te werk en vertrouw op instinct",
+        "Ik ga er vol in met brute kracht",
+        "Ik reageer explosief en snel"
+    ],
+    [
+        "Ik manipuleer en overdenk elke stap",
+        "Ik speel met hun angst en creëer chaos",
+        "Ik val direct aan zonder twijfel",
+        "Ik overlaad ze met pure energie"
+    ],
+    [
+        "Mechanische armen en superintelligentie",
+        "Glider‑technologie en bommen",
+        "Symbioot‑kracht en brute agressie",
+        "Elektriciteit beheersen"
+    ],
+    [
+        "Ik bewijs mijn superioriteit met strategie",
+        "Ik word onvoorspelbaar en gevaarlijk",
+        "Ik word woedend en ga in de aanval",
+        "Ik knal door het dak van frustratie"
+    ],
+    [
+        "Mijn ego en drang naar controle",
+        "Mijn paranoia en wantrouwen",
+        "Mijn woede en agressie",
+        "Mijn instabiliteit en impulsiviteit"
+    ],
+    [
+        "Met een plan en technologie",
+        "Alleen, zodat ik alles zelf kan bepalen",
+        "Als lone wolf, niemand komt dichtbij",
+        "Chaotisch en snel, zonder regels"
+    ],
+    [
+        "Ik test ze uitgebreid en onderzoek alles",
+        "Ik gebruik ze om chaos te verspreiden",
+        "Ik gebruik ze direct in een gevecht",
+        "Ik laat ze meteen los op alles om me heen"
+    ],
+    [
+        "Ik analyseer wat misging en verbeter mezelf",
+        "Ik zoek wraak en laat chaos achter",
+        "Ik word agressief en ga harder vechten",
+        "Ik verlies controle en ontplof"
+    ],
+    [
+        "Strategie en controle",
+        "Onvoorspelbaarheid en intimidatie",
+        "Pure kracht en agressie",
+        "Snelheid en overweldigende energie"
+    ]
+]
+
+
